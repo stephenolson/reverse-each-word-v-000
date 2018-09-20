0062668f098a3d6234c9e57638ff9ac139d7ec2d
Stephen Olson <stephenolson@gmail.com>
2018-09-20 22:21:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(sentence)
   new_array.join(" ")
 end
 
-def reverse_each_word_collect(sentence)
+def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = [ ]
   array.collect do |words|
