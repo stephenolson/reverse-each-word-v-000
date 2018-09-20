d341090054a42ef67a22a1fd787898fad84eb1e8
Stephen Olson <stephenolson@gmail.com>
2018-09-20 22:01:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   reverse = [ ]
-  toppings.each do |words|
-    reverse << "I love #{topping} on my burgers!"
+  sentence.each do |words|
+    reverse << words
   end
 
   
