f265d2044eea7deea6adecd3ad0cc0c39fc9472d
Stephen Olson <stephenolson@gmail.com>
2018-09-20 22:22:30 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  new_array = [ ]
+  array.each do |words|
+    new_array << words.reverse
+  end
+  new_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  new_array = [ ]
+  array.collect do |words|
+    new_array << words.reverse
+  end
+  new_array.join(" ")
+end
