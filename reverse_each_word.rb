40ad31d873cc0470435a848944d3ac2699657b49
Stephen Olson <stephenolson@gmail.com>
2018-09-20 22:21:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
-def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  new_array = [ ]
-  array.each do |words|
-    new_array << words.reverse
-  end
-  new_array.join(" ")
-end
+
 
 def reverse_each_word(sentence)
   array = sentence.split(" ")
