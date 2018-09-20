9314a40fe0383e8b47cc545686f5c5c654a450f4
Stephen Olson <stephenolson@gmail.com>
2018-09-20 22:13:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   forwards = sentence.split(" ")
   backwards = [ ]
   forwards.each do |words|
-    backwards << sentence.reverse
+    backwards.push sentence.reverse
   end
   backwards.join(" ")
 end
